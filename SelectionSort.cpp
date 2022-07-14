ea65ac70a06a4b1f180bd17438be5d0372e59bba
Neil D'Souza <neil.dsouza@servicenow.com>
2022-07-14 21:08:07 +1000

    Add .gitignore

---
+++ SelectionSort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <iterator>
+
+int findMin(int array[], int start, int arraySize);
+void printArray(int array[], int arraySize);
+
+int main()
+{
+    int unsortedArray[]{ 4, 7, 2, 30, 50, 20, 10, 40 };
+
+    //selection sort
+    int arraySize { std::size(unsortedArray) };
+    for (int x { 0 } ; x < arraySize; ++x){
+        int minPos = findMin(unsortedArray, x, arraySize);
+
+        //could use std::swap.
+        int temp = unsortedArray[minPos];
+        unsortedArray[minPos] = unsortedArray[x];
+        unsortedArray[x] = temp;
+    }
+    printArray(unsortedArray, arraySize);
+    return 0;
+}
+
+int findMin(int array[], int start, int arraySize){ 
+    int min { array[start] };
+    int pos = 0;
+
+    for (int i {start}; i < arraySize; ++i) {
+        if (array[i] < min) {
+            min = array[i];
+            pos = i;
+        }
+    }
+    pos = (min == array[start]) ? start : pos;
+    return pos;
+}
+
+void printArray(int array[], int arraySize) {
+    for (int i { 0 } ; i < arraySize; ++i){
+        std::cout << array[i] << " ";
+    }
+    std::cout << "\n";
+}
